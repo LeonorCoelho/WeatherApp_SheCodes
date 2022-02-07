1856a28e2f8a18d507effca75812b5b0c94c4a9d
Leonor Coelho <leonor_coelho@outlook.com>
2022-02-07 15:16:13 +0000

    ADD the JavaScript File
    Already done:
    Search City
    (Actual Day,
    Month, Temperature,  Temp Max and Min.)
    
    Missing doing:
    - the 4 days forecast;
    - emojis relatives with the temperature;

---
+++ src/script.js
@@ -0,0 +1,70 @@
+function weekInfo() {
+  let weekDays = document.querySelector("#weekDay");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  weekDays.innerHTML = day;
+}
+
+function dayMonthHourInfo() {
+  let dayMonth = document.querySelector("#dayMonth");
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  dayMonth.innerHTML = date + " " + month + " " + hours + ":" + minutes;
+}
+
+function showTemp(response) {
+  document.querySelector("#cityName-Principal").innerHTML = response.data.name;
+  document.querySelector("#temp-days").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "67913d7d175725fbd0ee22887fbda235";
+  let city = document.querySelector("#enter-city").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+let now = new Date();
+let cityInput = document.querySelector("#submit-city");
+weekInfo(now);
+dayMonthHourInfo(now);
+cityInput.addEventListener("submit", search);
